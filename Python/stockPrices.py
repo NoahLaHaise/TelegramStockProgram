8961e17f4380b5d799184421754eeab16943b63c
NoahLaHaise <noah.lahaise1@gmail.com>
2026-02-01 19:22:13 -0500

    pychanges

--- Python/stockPrices.py
+++ Python/stockPrices.py
@@ -28,34 +28,29 @@ class Stock:
     quickRatio: float
     priceHistory: list[float]
 
-    #def priceHistory(self, history):
-        #print(self.stockHistory)
-        #for price in self.price:
-            #print()
-       #self.stockHistory = history
-
-
-def main(stockName: str):
-
-    stock_info = yf.Ticker(stockName).info
-
+def get_longterm_history(stockName: str):
     price_history = yf.Ticker(stockName).history(period='3y', # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
                                    interval='1d', # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
                                    actions=False)
     
-
-    currStock = Stock(stockName, stock_info.get('currentPrice'), stock_info.get('earningsGrowth'), stock_info.get('profitMargins'), stock_info.get('ebitdaMargins'), stock_info.get('returnOnEquity'), stock_info.get('trailingEps'), stock_info.get('beta'), stock_info.get('pegRatio'), stock_info.get('totalCash'), stock_info.get('totalDebt'), stock_info.get('quickRatio'), price_history)  
-
     price_history['Return'] = (price_history['Close'] - price_history['Open']) / price_history['Open']
     price_history.drop(columns=['Low', 'High'], inplace=True)
     price_history[['Open', 'Close', 'Return']] = price_history[['Open', 'Close', 'Return']].round(4)
-    
+
     save_to_csv(price_history, stockName)
 
+def get_day_movement(stockName: str) -> None:
+    price_history = yf.Ticker(stockName).history(period='1d', # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+                                interval='5m', # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
+                                actions=False)
+    
+    price_history.drop(columns=['Low', 'High', 'Open'], inplace=True)
+    price_history[['Close']] = price_history[['Close']].round(4)
+    save_to_csv(price_history, stockName, False, "ShortTerm") 
 
-def save_to_csv(stock_history : pd.DataFrame, stock_name: str):
-    file_path = config.CSV_FILE_PATH / f"{stock_name}.csv"
-    stock_history.to_csv(file_path)
+def save_to_csv(stock_history : pd.DataFrame, stock_name: str, time_frame: str = "LongTerm"):
+    file_path = config.CSV_FILE_PATH / time_frame / f"{stock_name}.csv"
+    stock_history.to_csv(file_path) #set index =False to remove dateTime
 
 def generateNews(info, name) -> None:
     news = yf.Ticker(name).news
@@ -77,27 +72,13 @@ def calcBlackScholes(info, name)-> None:
     print("working")
 
 
-def displayInfo(stockInfo : Stock, name : str)-> None:
-    print("Valid periods include: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max\n")
-    per = input("Enter the period of time you would like your graph: ")
-    print("\n valid intervals include: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo")
-    inter = input("Enter the interval of prices you would: ")
-
-    price_history = yf.Ticker(name).history(period=per, # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
-                                   interval= inter, # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
-                                   actions=False)
-
-    print(stockInfo.name, ' ', stockInfo.price)
-
-    timeSeries = list(price_history['Open'])
-    dt_list = [pendulum.parse(str(dt)).float_timestamp for dt in list(price_history.index)]
-
-    plt.style.use('dark_background')
-    plt.plot(dt_list, timeSeries, linewidth=2)
-
-    plt.show()
-
-
 if len(sys.argv) > 1:
     stock = sys.argv[1]
-    main(stock)
+
+    if len(sys.argv) > 2:
+        mode = sys.argv[2]
+        if mode == "long":
+            get_longterm_history(stock)
+        else:
+            get_day_movement(stock) 
+            
