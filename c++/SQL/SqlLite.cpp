333ad194b7b5a38ecc03596ba2c2f7cbd3ada5de
NoahLaHaise <noah.lahaise1@gmail.com>
2026-03-08 21:01:35 -0400

    sql lite setup

---
+++ c++/SQL/SqlLite.cpp
@@ -0,0 +1,68 @@
+#include "SqlLite.h"
+#include <sqlite3.h>
+#include <iostream>
+
+SqlConnector::SqlConnector(const std::string& db_name) : database_name(db_name)
+{
+    sqlite3* db = get_connection();
+    if(!db)
+    {
+        std::cerr << "db connection failed";
+        return;
+    }
+    char* errMsg = nullptr;
+
+    const char* sql = "CREATE TABLE IF NOT EXISTS stock_analysis ("
+                    "id   INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    "stock TEXT NOT NULL,"
+                    "day_200_mean REAL,"
+                    "day_50_mean REAL"
+                    ");";
+
+    if (sqlite3_exec(db, sql, nullptr, nullptr, &errMsg) != SQLITE_OK) {
+        std::cerr << "Create table failed: " << errMsg << "\n";
+        sqlite3_free(errMsg);
+    }
+    sqlite3_close(db);
+}
+sqlite3* SqlConnector::get_connection()
+{
+    sqlite3* db;
+    int rc = sqlite3_open(database_name.c_str(), &db);  // Creates file if it doesn't exist
+
+    if (rc != SQLITE_OK) {
+        sqlite3_close(db);
+        return nullptr;
+    }
+    return db;
+}
+
+bool SqlConnector::insert_record(std::string stock_symbol, float large_sma, float small_sma)
+{
+    sqlite3* db = get_connection();
+    if(!db)
+    {
+        std::cerr << "db connection failed";
+        return false;
+    }
+
+    sqlite3_stmt* stmt;
+
+    sqlite3_prepare_v2(db, "INSERT INTO stock_analysis (stock, day_200_mean, day_50_mean) VALUES (?, ?, ?);", -1, &stmt, nullptr);
+
+    sqlite3_bind_text(stmt, 1, stock_symbol.c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_double(stmt, 2, large_sma);
+    sqlite3_bind_double(stmt, 3, small_sma);
+
+    if (sqlite3_step(stmt) != SQLITE_DONE) {
+        std::cerr << "Insert failed: " << sqlite3_errmsg(db) << "\n";
+        sqlite3_finalize(stmt);
+        sqlite3_close(db);
+        
+        return false;
+    }
+
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
+    return true;
+}
