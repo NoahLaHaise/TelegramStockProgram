c6fa30354d9874b1e388cd6deda17b81d0bb4979
NoahLaHaise <noah.lahaise1@gmail.com>
2026-04-12 22:25:41 -0400

    comment

--- Python/App.py
+++ Python/App.py
@@ -9,6 +9,7 @@ def send_alert():
     tele.send_message(msg)
 
 def daily_chat():
+    #TODO - pass in VWAP, RSI, moving averages 
     tele = TelegramMessenger()
     prompt = f"Provide an update on my stock portfolio for each individual stock. How is the price action today? Why are they moving? Are they moving because of the broader market? Or independent news? Provide an overview on the overall market structure and sentiment as well. If theres any major market wide news or company specific news that affect their future outlook, be sure to highlight it. {' '.join(WATCHLIST)}"
     gemini = GeminiAPI()
